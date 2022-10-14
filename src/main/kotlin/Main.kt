2e0f60b09975c1c3b6c507aecb5207afb9019100
David <datober70@mail.ru>
2022-10-14 20:24:27 +0300

    pract2

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,50 @@
+fun main(args: Array<String>) {
+    //Задача 1
+    val age1 = 42
+    val age2 = 21
+    println("Задача1")
+    println(age1)
+    println(age2)
+
+    //Задача 2
+    val avg = (age1+age2)/2
+    println ("Задача2")
+    println (avg)
+
+    //Задача 3
+    /*
+    Я немного не понял смысл задания, так как не нашёл ошибку попросту
+    */
+
+    //Задача 4
+    val firstName = "David"
+    val lastName = "Khazardzhyan"
+
+    // Задача 5
+    val FullName = firstName +" "+ lastName
+    println("Задача 5")
+    println (FullName)
+
+    //Задача 6
+    val MyDetails = ("Привет, меня зовут $FullName ")
+    println ("Задача 6")
+    println(MyDetails)
+
+    //Задача 7
+    val Triple: Triple< Int, Int, Int  > = Triple(10, 10, 2022)
+
+    //Задача 8
+    val day = Triple.first
+    val month = Triple.second
+    val years = Triple.third
+
+    /*
+    Совсем не понимаю что требуется сделать в 9 и 10 задачах
+    */
+    //Задача 9
+
+    println (month)
+    println( years)
+
+    //Задача 10 (Я не знаю как сделать)
+    val Pair
